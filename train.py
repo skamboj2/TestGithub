98f25b5a65b0b9219e325c0774817843af955524
skamboj2 <kamboj.sagar88@gmail.com>
2025-12-23 20:17:04 +0530

    Added DT model

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score, classification_report
 import pickle
@@ -11,7 +11,7 @@ X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create and train the model
-model = LogisticRegression(max_iter=200)
+model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 with open('model.pkl', 'wb') as f:
